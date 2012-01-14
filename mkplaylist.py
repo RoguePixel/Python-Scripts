f3c3a14698d3af4f86c84e4a0545226c8a571541
Rafael Loveszy <arqueangelg@gmail.com>
2012-01-14 15:46:55 -0500

    python script to recursively search for audio files in various formats in a given dir and append found files to an m3u playlist file. (finds files very fast!)

---
+++ mkplaylist.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from sys import argv as arg
+import glob, os, os.path, fnmatch
+
+###TODO
+#add control statements to take runtime multiple arguments
+#add option to name playlist file (not needed)
+#add function to find duplicate listings and or defunct file listing
+###TODO
+
+
+cwd=os.getcwd()
+aformats=('*.mp3','*.aac','*.flac','*.wav','*.m4a')
+playlist=open('playlist.m3u','a')
+home=os.getenv('HOME')
+fdir=' '.join(arg[1:])
+#musicdir=' '.join(arg[1:])
+#os.path.splitext(os.path.join(path,name))
+
+
+def listaudiofiles(root=os.getcwd()):
+	for path, dirs, files in os.walk(root):		
+		for name in files:
+			yield os.path.join(path,name)
+################################################
+
+if len(arg)==1:
+	#fdir=cwd
+	#print "print searching for audio in %s" % cwd
+	print "specify directory"
+	
+for f in listaudiofiles(fdir):	
+	ext=os.path.splitext(f)
+	for format in aformats: 
+		if "*"+ext[1]==format:
+			f=str(f)
+			#print f#, ext[1]
+			playlist.write("%s\n" % f)
+
+playlist.close
+
+
+
+
